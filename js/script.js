bac60b2cb4b2bcf3ae79864e2afdfce6db1eebd6
Nimplex <39964594+Nimplex@users.noreply.github.com>
2020-10-18 20:46:14 +0200

    code cleanup

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ window.onload = async() => {
   const temp  = document.getElementById('temp')
   const nick  = document.getElementById('text')
   const search= document.getElementById('searchbox')
-  let nickname = await localStorage.getItem('nick')
-  let api_key  = await localStorage.getItem('api-key')
+  let nickname= await localStorage.getItem('nick')
+  let api_key = await localStorage.getItem('api-key')
 
-  if (nickname == 'null') nickname = 'guest'
+  nickname ? nickname: 'guest'
+  api_key  ? api_key: '0'
+  
   if (!api_key) {
     const api_key = await prompt('Please provide weather api key from: https://openweathermap.com/api or 0')
     const nickname = await prompt('Please provide your nickname')
@@ -27,18 +29,16 @@ window.onload = async() => {
     }
   }
   updateClock = () => {
-    const time = `${new Date().getHours() < 10 ? `0${new Date().getHours()}` : new Date().getHours()}:${new Date().getMinutes() < 10 ? `0${new Date().getMinutes()}` : new Date().getMinutes()}`
+    const date = new Date()
+    const time = `${date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()}:${date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()}`
     clock.innerText = time
   }
-  updateNick = () => {
-    nick.innerHTML = nick.innerHTML.replace(/\[nick\]/, nickname)
-  }
+  updateNick = () => nick.innerHTML = nick.innerHTML.replace(/\[nick\]/, nickname)
   updateDate = () => {
-    date.innerHTML = `${new Date().toString().split(' ')[2]} <span class="dataSpan">${new Date().toString().split(' ')[1]}</span>`
+    const dat = new Date()
+    date.innerHTML = `${dat.toString().split(' ')[2]} <span class="dataSpan">${dat.toString().split(' ')[1]}</span>`
   }
-  search.addEventListener('keydown', (e) => {
-    if (e.key == 'Enter') location.replace(`https://duckduckgo.com/?q=${search.value}`)
-  })
+  search.addEventListener('keydown', (e) => e.key == 'Enter'? location.replace(`https://duckduckgo.com/?q=${search.value}`): '')
   updateClock()
   updateWeather()
   updateNick()
