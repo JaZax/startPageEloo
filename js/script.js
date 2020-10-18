43d1d20b980dc46a4bf45b2157c1c383e4a9e146
Nimplex <39964594+Nimplex@users.noreply.github.com>
2020-10-18 18:57:12 +0200

    small changes, added event to searchbox

--- js/script.js
+++ js/script.js
@@ -3,17 +3,18 @@ window.onload = async() => {
   const date  = document.getElementById('date')
   const temp  = document.getElementById('temp')
   const nick  = document.getElementById('text')
-  const nickname = await localStorage.getItem('nick')
-  const api_key  = await localStorage.getItem('api-key')
+  const search= document.getElementById('searchbox')
+  let nickname = await localStorage.getItem('nick')
+  let api_key  = await localStorage.getItem('api-key')
 
-  if (!localStorage.getItem('api-key') || !localStorage.getItem('nick')) {
-    const api_key = await prompt('Please provide weather api key from: https://openweathermap.com/api')
+  if (nickname == 'null') nickname = 'guest'
+  if (!api_key) {
+    const api_key = await prompt('Please provide weather api key from: https://openweathermap.com/api or 0')
     const nickname = await prompt('Please provide your nickname')
     localStorage.setItem('api-key', api_key)
     localStorage.setItem('nick', nickname)
     location.reload()
   }
-
   updateWeather = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(pos => {
@@ -35,6 +36,9 @@ window.onload = async() => {
   updateDate = () => {
     date.innerHTML = `${new Date().toString().split(' ')[2]} <span class="dataSpan">${new Date().toString().split(' ')[1]}</span>`
   }
+  search.addEventListener('keydown', (e) => {
+    if (e.key == 'Enter') location.replace(`https://duckduckgo.com/?q=${search.value}`)
+  })
   updateClock()
   updateWeather()
   updateNick()
